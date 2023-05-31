d7ef8b7e9a81fdbbd45ce612318b8de286ff5851
Abizar <abizararrifai@gmail.com>
2023-05-31 12:34:15 +0700

    modifikasi

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const { log } = require("console");
 var express = require("express");
 var app = express();
 
@@ -24,40 +25,40 @@ function logger(req, res, next) {
   next();
 }
 
-// // calls logger:middleware for each requeste-response cycle
+// calls logger:middleware for each requeste-response cycle
 app.use(logger);
 
-// // route that gets executed for the path '/'
+// route that gets executed for the path '/'
 app.get("/", function (req, res) {
   res.send("This is a basic example for express.js");
 });
 
-app.get("/api/users", function (req, res) {
-  const user_id = req.query.id;
-  const token = req.query.token;
-  const geo = req.query.geo;
+// app.get("/api/users", function (req, res) {
+//   const user_id = req.body.id;
+//   const token = req.body.token;
+//   const geo = req.body.geo;
 
-  res.send({
-    user_id: user_id,
-    token: token,
-    geo: geo,
-  });
-});
+//   res.send({
+//     user_id: user_id,
+//     token: token,
+//     geo: geo,
+//   });
+// });
 
 app.get("/api/:version", function (req, res) {
   res.send(req.params.version);
 });
 
-app.param("name", function (req, res, next, name) {
-  const modified = name.toUpperCase();
+// app.param("name", function (req, res, next, name) {
+//   const modified = name.toUpperCase();
 
-  req.name = modified;
-  next();
-});
+//   req.name = modified;
+//   next();
+// });
 
-app.get("/api/users/:name", function (req, res) {
-  res.send("Hello " + req.name + "!");
-});
+// app.get("/api/users/:name", function (req, res) {
+//   res.send("Hello " + req.name + "!");
+// });
 // app.listen(port);
 // console.log("server started at http://localhost:" + port);
 
@@ -66,18 +67,20 @@ const port = process.env.PORT || 8080;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.post("/api/users", function (req, res) {
-  const nama = req.body.nama;
-  const tempat_lahir = req.body.tempat_lahir;
-  const tanggal_lahir = req.body.tanggal_lahir;
-  const alamat = req.body.alamat;
+app.get("/biodata", function (req, res) {
+  const nama = req.query.nama;
+  const tempat_lahir = req.query.tempat_lahir;
+  const tanggal_lahir = req.query.tanggal_lahir;
+  const alamat = req.query.alamat;
 
-  res.send({
+  const biodata = {
     nama,
     tempat_lahir,
     tanggal_lahir,
     alamat,
-  });
+  };
+
+  res.json(biodata);
 });
 // Start the server
 app.listen(port);
